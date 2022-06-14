0d42c006c6a7d259592c99bcf24d763bdacdda27
FelipeCampos14 <99193547+FelipeCampos14@users.noreply.github.com>
2022-06-14 10:48:25 -0300

    Ajustes Aula

---
+++ 03_AUT_EST_ENTREGA/Semana 8/Currículo/backend/app_08.js
@@ -0,0 +1,86 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3081;
+const sqlite3 = require('sqlite3').verbose(); 
+const DBPATH = 'dbCurriculo.db'; 
+
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static("../frontend/"));
+
+app.use(express.json());
+
+/* Definição dos endpoints */
+
+/******** CRUD ************/
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM tbCurriculo ORDER BY email COLLATE NOCASE';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO tbCurriculo (email, name) VALUES ('" + req.body.email + "','"+ req.body.name+"')";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE tbCurriculo SET email = '" + req.body.email + "' WHERE name = " + req.body.name;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM tbCurriculo WHERE email = '"+req.body.email+"'";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Page server running at http://${hostname}:${port}/`);
+});
